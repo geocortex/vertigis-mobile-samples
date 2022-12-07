4185177b619a2a36890dccfd78c3c291aaf579f9
Jeff Siemens <jeff.siemens@vertigis.com>
2022-12-06 16:24:41 -0800

    Updated VertiGIS.Mobile package to 5.21 Updated button styles to not show title Updated arguments for "sketching.set-interaction-mode" Updated android project settings Fixed issue with Selector.xaml on iOS so that it's full height instead of just centered

--- Geocortex.Mobile.Samples/Geocortex.Mobile.Samples/Samples/Workflow/CustomFormComponent/CustomFormComponent.xaml.cs
+++ Geocortex.Mobile.Samples/Geocortex.Mobile.Samples/Samples/Workflow/CustomFormComponent/CustomFormComponent.xaml.cs
@@ -6,7 +6,7 @@ namespace VertiGIS.Mobile.Samples.Samples.Workflow.CustomFormComponent
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CustomFormComponent : ContentComponent
     {
-        public CustomFormComponent(Geocortex.Workflow.Runtime.Definition.Forms.Element element, string name)
+        public CustomFormComponent(VertiGIS.Workflow.Runtime.Definition.Forms.Element element, string name)
             : base(element, name)
         {
             InitializeComponent();
